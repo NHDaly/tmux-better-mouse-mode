bf00d5ac52a76d34ddcae7b73c8acd689d5b0142
Nathan Daly <nhdaly@gmail.com>
2016-09-28 02:03:20 -0500

    Added tests that the scroll speed works correctly
    Currently only tests integer scroll speeds.

--- tests/basic_functionality_test.sh
+++ tests/basic_functionality_test.sh
@@ -1,14 +1,17 @@
 #!/usr/bin/env sh
 
 # Setup
+#tmux() {
+#  # Forward the "getter" calls to actual tmux, but change the setters to just
+#  # echo, so we can test what its setting.
+#  if [[ "$1" == "show-option" ]] ; then
+#    tmux-2.2/tmux "$@"
+#  else 
+#    echo "$@"
+#  fi
+#}
 tmux() {
-  # Forward the "getter" calls to actual tmux, but change the setters to just
-  # echo, so we can test what its setting.
-  if [[ "$1" == "show-option" ]] ; then
-    tmux-2.2/tmux "$@"
-  else 
-    echo "$@"
-  fi
+  tmux-2.2/tmux "$@"
 }
 export -f tmux
 
@@ -16,11 +19,35 @@ export -f tmux
 
 
 testBasicBindKeysAreCalled() {
+  bash scroll_copy_mode.tmux
+
+  assertNotNull "`tmux list-keys -T root | grep 'WheelUpPane'`"
+  assertNotNull "`tmux list-keys -T root | grep 'WheelDownPane'`"
+}
+
+checkScrollSpeedSetCorrectly() {
+  testSpeed="$1"
+  expectedValue="$2"
+  tmux set -g @scroll-speed-num-lines-per-scroll $testSpeed
+
   settings_output=$(bash scroll_copy_mode.tmux)
 
-  assertNotNull "`echo $settings_output | grep 'bind-key -n WheelUpPane'`"
-  assertNotNull "`echo $settings_output | grep 'bind-key -n WheelDownPane'`"
+  assertEquals 'number of `send-keys` per scroll not equal to user setting' $expectedValue `tmux list-keys -T root | grep 'WheelUpPane' | grep -o "'send-keys[^']*'" | head -n 1 | grep -o 'send-keys' | wc -l`
 }
+
+testValidIntegerScrollSpeeds() {
+  checkScrollSpeedSetCorrectly 1 1
+  checkScrollSpeedSetCorrectly 2 2
+  checkScrollSpeedSetCorrectly 10 10
+  checkScrollSpeedSetCorrectly 0 0
+}
+
+testInvalidIntegerScrollSpeeds() {
+  checkScrollSpeedSetCorrectly "-1" 0
+  checkScrollSpeedSetCorrectly "-10" 0
+  checkScrollSpeedSetCorrectly "-0.1" 0
+}
+
 
 # --------- Run tests command -------------
 . shunit2-2.1.6/src/shunit2
